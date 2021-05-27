c7b2ada40e0d71084c0b837912c6dd0b95d3ce97
Angelique De Castro <angeliqueajdecastro@gmail.com>
2021-05-27 16:17:13 -0400

    demo prep

--- script.js
+++ script.js
@@ -6,17 +6,32 @@ let count = 0;
 const ids = ['first', 'second', 'third'];
 
 const setCardUrl = async () => {
-  const asset = await fetch('/asset');
+  let asset;
+
+  // demo purposes
+  if (document.location.href.includes('serial-magic/index.html')) {  
+    if (count%3 == 0) asset = 'https://ih1.redbubble.net/image.350564324.7307/flat,800x800,075,f.u1.jpg';
+    if (count % 3 == 1) asset = 'https://i.pinimg.com/736x/23/1f/49/231f49d0d200ebcb63d35f381be24a81.jpg';
+    if (count % 3 == 2) asset = 'https://i.pinimg.com/originals/8a/2d/2e/8a2d2e26d93a2d619401a0e8af8ec09e.jpg';
+  } else {
+    asset = await fetch('/asset');
+  }
+
   console.log(asset);
   document.getElementById(ids[count%3]).src = asset;
 };
 
 const displayCard = (value) => {
   console.log(value);
-  if (value.includes('UID Value')) {
+  if (value.includes('UID')) {
     setCardUrl();
     ++count;
   }
+};
+
+const hideControls = () => {
+  const controls = document.getElementById('controls');
+  controls.style.display = 'none';
 }
 
 // Listen to data coming from the serial device.
@@ -51,6 +66,8 @@ async function connect() {
 
   // Wait for the serial port to open.
   await port.open({ baudRate: 115200 });
+
+  hideControls();
 
   listen(port);
 }
